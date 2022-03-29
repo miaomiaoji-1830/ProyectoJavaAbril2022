d90bfe8310a072f3fb387fd8a711293187ac8728
Miao <640645590m@gmail.com>
2022-03-29 19:01:33 +0200

    Esto es el comentario del Ejercicio1 del EjemploJava2002

---
+++ src/main/java/EjemploJava1/Ejercicio1.java
@@ -0,0 +1,31 @@
+package EjemploJava1;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+      LocalDateTime Exuni = LocalDateTime.of(1900, 11, 12, 0, 0);
+		LocalDateTime Actual = LocalDateTime.now();
+
+		long dias = ChronoUnit.DAYS.between(Exuni, Actual);
+		
+		System.out.println("====DIAS DE FINALIZACION DE LA EXPOSICION UNIVERSAL DE PARIS HASTA HOY====");
+		System.out.println("Los dias usado: " + dias + " dias");
+		
+		
+		LocalDateTime PGuerra = LocalDateTime.of(1914, 7, 28, 14, 00);
+		LocalDateTime actual = LocalDateTime.now();
+		long minuto = ChronoUnit.MINUTES.between(PGuerra, actual);
+
+		System.out.println("\n====MINUTOS DE PRIMERA GUERRA MUNDIAL HASTA HOY====");
+		System.out.println("Los minutos usado: " + minuto + " minutos");
+		long transcurrimin = ChronoUnit.MINUTES.between(Exuni, PGuerra);
+		long transcurrihor = ChronoUnit.HOURS.between(Exuni, PGuerra);
+		
+		System.out.println("\n====MINUTOS Y HORAS ENTRE DOS EVENTO====");
+		System.out.println("Los minutos usado: " + transcurrimin + " minutos");
+		System.out.println("Los horas usado: " + transcurrihor + " horas");
+    }
+    
+}
